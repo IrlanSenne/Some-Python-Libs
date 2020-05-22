0371e72214eb7881717b03524555716f2cf6990d
IrlanSenne <im.senne2017@gmail.com>
2020-05-22 14:41:22 +0100

    Segunda parte concluída

--- jogo da velha.py
+++ jogo da velha.py
@@ -5,7 +5,7 @@ from colorama import Fore, Back
 jogarNovamente="s"
 jogadas=0
 quemJoga=2
-maxJogada=9
+maxJogadas=9
 vit="n"
 velha=[
     [" "," "," "],
@@ -23,6 +23,43 @@ def tela():
     print()
     print(f"Jogadas: " + Fore.GREEN +  str(jogadas) +  Fore.RESET)
     print()
+
+def jogadorJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga==2 and jogadas<maxJogadas:       
+        try:
+            l=int(input("Linha...: "))
+            c=int(input("Coluna..: "))
+            while velha[l][c] != " ":
+                l=int(input("Linha...: "))
+                c=int(input("Coluna..: "))
+            velha[l][c]="X"
+            quemJoga=1
+            jogadas+=1
+        except:
+            print("Linha e/ou coluna inváida")
+            os.system("pause")
+            
+def cpuJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga==1 and jogadas<maxJogadas:
+        l=random.randrange(0,3)
+        c=random.randrange(0,3)
+        while velha[l][c] != " ":
+            l=random.randrange(0,3)
+            c=random.randrange(0,3)
+        velha[l][c]="O"
+        jogadas+=1
+        quemJoga=2
+
+while True:
+    tela()
+    jogadorJoga()
+    cpuJoga()
+
     
 
-tela()
