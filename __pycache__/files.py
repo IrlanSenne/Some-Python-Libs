ccfe6ea6225992015dfe353dfad0c13348d27194
IrlanSenne <im.senne2017@gmail.com>
2020-05-26 11:34:49 +0100

    FinisheFiles

---
+++ __pycache__/files.py
@@ -0,0 +1,24 @@
+f=open("C:/Users/imsen/OneDrive/Área de Trabalho/Development/python/createdFile.txt", "rt")
+
+# r - read
+# a - append
+# w - write
+# x - create
+
+# t - texto
+# b - binario
+"""
+f.write("Testing")
+
+txt=input("Type a text ")
+f.write(txt + '\n')
+
+os.remove("anything")
+"""
+
+
+print(f.read())
+
+
+
+f.close()
