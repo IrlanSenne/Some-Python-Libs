7e69eec126c9af4788b4ce1ee8d62ea6d12f2026
IrlanSenne <im.senne2017@gmail.com>
2020-05-27 09:21:17 +0100

    Completed Methods

--- SQLiteStudio/agenda.py
+++ SQLiteStudio/agenda.py
@@ -14,6 +14,23 @@ def ConexaoBanco():
     return con
 vcon=ConexaoBanco()
 
+def query(conexao,sql):
+    try:
+        c=conexao.cursor()
+        c.execute(sql)
+        conexao.commit()
+    except Error as er:
+        print(er)
+    finally:
+        print("Operação realizada com sucesso")
+        #conexao.close()
+
+def consultar(conexao,sql):
+    c=conexao.cursor()
+    c.execute(sql)
+    res=c.fetchall()
+    return res
+
 def MenuPricipal():
     os.system("cls")
     print("1 - Inserir novo registro")
@@ -24,13 +41,37 @@ def MenuPricipal():
     print("6 - Sair")
 
 def menuInserir():
-    print()
+    os.system("cls")
+    nome=input("Digite o nome : ")
+    telefone=input("Digite o telefone : ")
+    email=input("Digite o email : ")
+    vsql="INSERT INTO tb_contatos (T_NOMECONTATO, T_TELEFONECONTATO, T_EMAILCONTATO) VALUES ('"+nome+"','"+telefone+"','"+email+"')"
+    query(vcon, vsql)
 
 def menuDeletar():
-    print()
+    os.system("cls")
+    vid=input("Digite o ID do registro a ser deletado : ")
+    vsql="DELETE FROM tb_contatos WHERE N_IDCONTATO="+vid
+    query(vcon, vsql)
 
 def menuAtualizar():
-    print()
+    os.system("cls")
+    vid=input("Digite o ID do registro a ser atualizado : ")
+    r=consultar(vcon,"SELECT * FROM tb_contatos WHERE N_IDCONTATO="+vid)
+    rnome=r[0][1]
+    rtelefone=r[0][2]
+    remail=r[0][3]
+    nome=input("Digite o nome : ")
+    telefone=input("Digite o telefone : ")
+    email=input("Digite o email : ")
+    if len(nome) == 0 :
+        nome=rnome
+    if len(telefone)== 0:
+        telefone=rtelefone
+    if len(email)== 0:
+        email=remail
+    vsql="UPDATE tb_contatos SET T_NOMECONTATO='"+nome+"', T_TELEFONECONTATO='"+telefone+"', T_EMAILCONTATO='"+email+"' WHERE N_IDCONTATO="+vid 
+    query(vcon,vsql)
 
 def menuConsultarId():
     print()
@@ -61,4 +102,6 @@ while opc != 6:
         os.system("cls")
         print("Opção inválida")
         os.system("pause")
+
+vcon.close()
 os.system("pause")
